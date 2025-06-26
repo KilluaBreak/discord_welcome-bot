fc0a63e2a2aff5681f34a8c0f6ce7a20b5ca5fb3
𝙺𝚒𝚕𝚕𝚞𝚊 <sulthanrabbani771@gmail.com>
2025-06-26 12:35:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ async def on_member_join(member):
     member_count = member.guild.member_count
     image = await generate_welcome_image(member, member_count)
     file = discord.File(fp=image, filename="welcome.png")
-    embed = discord.Embed(title="Welcome!", description=f"{member.mention} Welcome To YugenX\nkenalin aku Yugen yang membawamu untuk berkeliling di sever YugenX\n\npertama kamu verif dulu ya di <#VERIF>\nnah sekarang kita baca rule dulu yuk <#RULE>\nkalau kamu buth temen, kamu bisa kenalan di <#CHAT>\n\nEnjoy and Fun\nYugenX ┃ Style Your Isekai", color=discord.Color.purple())
+    embed = discord.Embed(title="Welcome!", description=f"{member.mention} Welcome To YugenX\nkenalin aku Yugen yang membawamu untuk berkeliling di sever YugenX\n\npertama kamu verif dulu ya di <#{VERIF}>\nnah sekarang kita baca rule dulu yuk <#{RULE}>\nkalau kamu buth temen, kamu bisa kenalan di <#{CHAT}>\n\nEnjoy and Fun\nYugenX ┃ Style Your Isekai", color=discord.Color.purple())
     embed.set_image(url="attachment://welcome.png")
     await channel.send(embed=embed, file=file)
 
